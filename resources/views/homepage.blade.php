53d8d321833324c247e8bcfd96cd7eddb2f84ed2
Rindho Samat <rindho.samat@students.ukdw.ac.id>
2019-02-28 16:09:30 +0700

    commit #2

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,88 +3,97 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Laravel</title>
-        <link rel="stylesheet" type="text/css" href="./css/bulma.min.css">
-        <link rel="stylesheet" type="text/css" href="./fontawesome/css/all.min.css">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
-        <header>
-            
-        </header>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        </a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
         </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-        <a class="navbar-item">
-        Home
-        </a>
-
-        <a class="navbar-item">
-        Documentation
-        </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-        More
-        </a>
-
-        <div class="navbar-dropdown">
-        <a class="navbar-item">
-        About
-        </a>
-        <a class="navbar-item">
-        Jobs
-        </a>
-        <a class="navbar-item">
-        Contact
-        </a>
-        <hr class="navbar-divider">
-        <a class="navbar-item">
-        Report an issue
-        </a>
-        </div>
-        </div>
-        </div>
-
-        <div class="navbar-end">
-        <div class="navbar-item">
-        <div class="buttons">
-        <a class="button is-primary">
-        <strong>Sign up</strong>
-        </a>
-        <a class="button is-light">
-        Log in
-        </a>
-        </div>
-        </div>
-        </div>
-        </div>
-        </nav>
-        <script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
-        <script type="text/javascript" src="./fontawesome/js/all.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
-});
-        </script>
     </body>
 </html>
